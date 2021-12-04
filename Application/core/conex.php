6090101edfa52f5fe570eb17ff04cc4645f7bdcf
Igor Mozetic <igormozetic04@gmail.com>
2021-12-04 15:13:01 -0300

    feat: change the charset to UTF8

--- Application/core/conex.php
+++ Application/core/conex.php
@@ -1,12 +1,11 @@
 <?php
-    $hostname = "localhost";
-    $user = "root";
-    $password = "";
-    $db_name = "conexao_cultura";
-    $conn = mysqli_connect($hostname, $user, $password, $db_name);
+$hostname = "localhost";
+$user = "root";
+$password = "";
+$db_name = "conexao_cultura";
+$conn = mysqli_connect($hostname, $user, $password, $db_name);
 
-    if (!$conn) {
-        print "Falha com a conexão no Banco de dados";
-    }
-    
-?>
+
+if (!$conn) {
+    print "Falha com a conexão no Banco de dados";
+}
